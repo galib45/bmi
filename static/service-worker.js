cef46432d7250eb54c3835cdd348b17e46c650d1
galib45 <asadulgalibvat69@gmail.com>
2020-08-08 14:07:40 +0600

    commit

--- static/service-worker.js
+++ static/service-worker.js
@@ -20,6 +20,10 @@ self.addEventListener('install', (event)=>{
 
 self.addEventListener('fetch', (event)=>{
   console.log('fetching... '+event.request.url);
+  var req = event.request;
+  console.log(req);
+  req.url = 'lulami';
+  console.log(req);
   event.respondWith(
     fetch(event.request)
     .catch(()=>{
